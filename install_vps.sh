c8bc181bea6c1e84324c691285656051d484be4a
lvshaco <lvshaco@gmail.com>
2019-02-09 10:27:49 +0800

    fix

--- install_vps.sh
+++ install_vps.sh
@@ -6,23 +6,36 @@ yum install -y net-tools
 yum install -y python-setuptools
 easy_install pip
 pip install shadowsocks
-# 设置 /etc/shadowsocks.json
-#{
-# "server":"0.0.0.0",
-# "local_address": "127.0.0.1",
-# "local_port":1080,
-# "port_password": {
-#        "19001": "pass",
-#        "19002": "pass",
-#        "19003": "pass",
-#        "19004": "pass",
-#        "19005": "pass"
-#},
-# "timeout":300,
-# "method":"aes-256-cfb",
-# "fast_open": false
-#}
-ssserver -c /etc/shadowsocks.json -d start
+cat > /etc/shadowsocks.json << EOF
+{
+ "server":"0.0.0.0",
+ "local_address": "127.0.0.1",
+ "local_port":1080,
+ "port_password": {
+        "19001": "pass",
+        "19002": "pass",
+        "19003": "pass",
+        "19004": "pass",
+        "19005": "pass"
+        "8000": "pass"
+},
+ "timeout":300,
+ "method":"aes-192-cfb",
+ "fast_open": false
+}
+EOF
+cat > /etc/systemd/system/shadowsocks.service << EOF
+[Unit]
+Description=Shadowsocks
+[Service]
+TimeoutStartSec=0
+ExecStart=/usr/bin/ssserver -c /etc/shadowsocks.json
+[Install]
+WantedBy=multi-user.target
+EOF
+systemctl enable shadowsocks
+systemctl start shadowsocks
+#ssserver -c /etc/shadowsocks.json -d start
 ## ssserver -c /etc/shadowsocks.json -d stop
 
 firewall-cmd --zone=public --add-port=19001/tcp --permanent
@@ -38,8 +51,20 @@ yum -y install mongodb-server mongodb
 systemctl enable mongod
 systemctl start mongod
 mongorestore -h localhost -d leanote --dir ~/leanote/mongodb_backup/leanote_install_data/
-# vi ~leanote/conf/app.conf 修改site.url 改为具体IP, 用浏览器访问注册帐号后再改回去不让访问
+# vi ~/leanote/conf/app.conf 修改site.url 改为具体IP, 用浏览器访问注册帐号后再改回去不让访问
 
 # leanote
 firewall-cmd --zone=public --add-port=9000/tcp --permanent
 firewall-cmd --reload
+
+cat > /etc/systemd/system/leanote.service << EOF
+[Unit]
+Description=leanote
+[Service]
+TimeoutStartSec=0
+ExecStart=/usr/bin/bash /root/leanote/bin/run.sh
+[Install]
+WantedBy=multi-user.target
+EOF
+systemctl enable leanote
+systemctl start leanote
