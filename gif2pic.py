271c83babf9a76e8b3eb6df57006c9a50be4a925
lvshaco <lvshaco@gmail.com>
2019-08-01 22:14:34 +0800

    fix

---
+++ gif2pic.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+#_*_ coding:utf-8 _*_
+
+# ---------------------------------------------------
+# 将input_path目录下的gif转化成png图片
+# 例如boss_walk.gif会转化成boss/walk/00.png ~ XX.png
+# ---------------------------------------------------
+
+import sys
+import os
+
+if len(sys.argv) < 3:
+    print "Usage: %s input_path output_path" % sys.argv[0]
+    sys.exit(1)
+
+input_path = sys.argv[1]
+output_path = sys.argv[2]
+
+for f in os.listdir(input_path):
+    f_ext = os.path.splitext(f)
+    ext = f_ext[-1]
+    if ext == ".gif":
+        f = os.path.join(input_path, f)
+        if not os.path.isdir(f):
+            fname = f_ext[0]
+            fname_l = fname.split("_")
+            out = "/".join(fname_l)
+            out = os.path.join(output_path, out)
+            if not os.path.exists(out):
+                os.makedirs(out)
+            cmd = "convert %s %s/%%02d.png"%(f, out)
+            print(cmd)
+            os.system(cmd)
