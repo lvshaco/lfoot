6e42288181c964b83fd7f0cb67b8bbde5d7cde08
lvshaco <lvshaco@gmail.com>
2020-04-05 23:10:48 +0800

    fix

--- install_vps.sh
+++ install_vps.sh
@@ -12,12 +12,12 @@ cat > /etc/shadowsocks.json << EOF
  "local_address": "127.0.0.1",
  "local_port":1080,
  "port_password": {
-        "19001": "pass",
-        "19002": "pass",
-        "19003": "pass",
-        "19004": "pass",
-        "19005": "pass"
-        "8000": "pass"
+        "19001": "abwiehs82%*&8202!29EFG",
+        "19002": "abwiehs82%*&8202!29EFG",
+        "19003": "abwiehs82%*&8202!29EFG",
+        "19004": "abwiehs82%*&8202!29EFG",
+        "19005": "abwiehs82%*&8202!29EFG",
+        "18000": "abwiehs82%*&8202!29EFG"
 },
  "timeout":300,
  "method":"aes-192-cfb",
@@ -43,28 +43,29 @@ firewall-cmd --zone=public --add-port=19002/tcp --permanent
 firewall-cmd --zone=public --add-port=19003/tcp --permanent
 firewall-cmd --zone=public --add-port=19004/tcp --permanent
 firewall-cmd --zone=public --add-port=19005/tcp --permanent
+firewall-cmd --zone=public --add-port=18000/tcp --permanent
 firewall-cmd --reload
 
-wget https://sourceforge.net/projects/leanote-bin/files/2.6.1/leanote-linux-amd64-v2.6.1.bin.tar.gz/download
-tar -xf download
-yum -y install mongodb-server mongodb
-systemctl enable mongod
-systemctl start mongod
-mongorestore -h localhost -d leanote --dir ~/leanote/mongodb_backup/leanote_install_data/
-# vi ~/leanote/conf/app.conf 修改site.url 改为具体IP, 用浏览器访问注册帐号后再改回去不让访问
-
-# leanote
-firewall-cmd --zone=public --add-port=9000/tcp --permanent
-firewall-cmd --reload
-
-cat > /etc/systemd/system/leanote.service << EOF
-[Unit]
-Description=leanote
-[Service]
-TimeoutStartSec=0
-ExecStart=/usr/bin/bash /root/leanote/bin/run.sh
-[Install]
-WantedBy=multi-user.target
-EOF
-systemctl enable leanote
-systemctl start leanote
+#wget https://sourceforge.net/projects/leanote-bin/files/2.6.1/leanote-linux-amd64-v2.6.1.bin.tar.gz/download
+#tar -xf download
+#yum -y install mongodb-server mongodb
+#systemctl enable mongod
+#systemctl start mongod
+#mongorestore -h localhost -d leanote --dir ~/leanote/mongodb_backup/leanote_install_data/
+## vi ~/leanote/conf/app.conf 修改site.url 改为具体IP, 用浏览器访问注册帐号后再改回去不让访问
+#
+## leanote
+#firewall-cmd --zone=public --add-port=9000/tcp --permanent
+#firewall-cmd --reload
+#
+#cat > /etc/systemd/system/leanote.service << EOF
+#[Unit]
+#Description=leanote
+#[Service]
+#TimeoutStartSec=0
+#ExecStart=/usr/bin/bash /root/leanote/bin/run.sh
+#[Install]
+#WantedBy=multi-user.target
+#EOF
+#systemctl enable leanote
+#systemctl start leanote
